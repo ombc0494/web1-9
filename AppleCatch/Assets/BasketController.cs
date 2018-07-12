9c003e5d6257d69220566ad00afce95552138eea
Unknown <g1727041@st.t-kougei.ac.jp>
2018-07-12 18:30:13 +0900

    悪魔的コミット

---
+++ AppleCatch/Assets/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour {
+
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource and;
+    GameObject director;
+
+    void Start()
+    {
+        this.director = GameObject.Find("GameDirector");
+        this.and = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Apple")
+        {
+            this.director.GetComponent<GameDirector>().GetApple();
+            this.and.PlayOneShot(this.appleSE);
+        }
+        else
+        {
+            this.director.GetComponent<GameDirector>().GetBomb();
+            this.and.PlayOneShot(this.bombSE);
+        }
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+		
+	}
+}
---
+++ AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+    GameObject timerText;
+    GameObject PointText;
+
+    float time = 60.0f;
+    int point = 0;
+
+    public void GetApple()
+    {
+        this.point += 100;
+    }
+    public void GetBomb()
+    {
+        this.point /= 2;
+    }
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+        this.PointText = GameObject.Find("Point");
+
+    }
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
+        this.PointText.GetComponent<Text>().text = this.point.ToString() + "point";
+
+    }
+}
+
